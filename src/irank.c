707e12ecaaba878d23450cf11618381cc65845d3
Torsten Hothorn <Torsten.Hothorn@rzmail.uni-erlangen.de>
2002-11-18 00:00:00 +0000

    version 0.7-2

--- src/irank.c
+++ src/irank.c
@@ -1,6 +1,6 @@
 /*
 
-  $Id: irank.c,v 1.1 2002/09/17 12:38:03 hothorn Exp $
+  $Id: irank.c,v 1.3 2002/11/18 10:33:30 hothorn Exp $
   
   irank: integer valued ranks
   modified from R-1.5.1/src/main/sort.c
@@ -13,8 +13,8 @@
 
 SEXP irank(SEXP x, SEXP orderx)
 {
-    SEXP rank;
-    double *rk, *tx;
+    SEXP rk;
+    double *tx;
     int *to;
     int i, j, k, n;
 
@@ -23,12 +23,11 @@ SEXP irank(SEXP x, SEXP orderx)
     n = LENGTH(x);
     if (!isVector(orderx)) /* || length(orderx) != n) */
         error("orderx is not a vector of the same length as x");
-    PROTECT(rank = allocVector(REALSXP, n));
+    PROTECT(rk = allocVector(INTSXP, n));
     UNPROTECT(1);
     if (n > 0) {
         tx = REAL(x);
         to = INTEGER(orderx);
-	rk = REAL(rank);
 	i = 0;
 	while (i < n) {
 	    j = i;
@@ -40,12 +39,12 @@ SEXP irank(SEXP x, SEXP orderx)
 		    return the number of observations 
 		    less or equal tx[to[k]]
 		    */
-		    rk[to[k]] = j + 1;
+		    INTEGER(rk)[to[k]] = j + 1;
 	    }
 	    else
-		rk[to[i]] = i + 1;
+		INTEGER(rk)[to[i]] = i + 1;
 	    i = j + 1;
 	}
     }
-    return rank;
+    return rk;
 }

bb30d97a91ba2e87ff338139798a9cbd022478c0
Torsten Hothorn <Torsten.Hothorn@rzmail.uni-erlangen.de>
2006-02-02 00:00:00 +0000

    version 0.8-13

--- R/zzz.R
+++ R/zzz.R
@@ -10,4 +10,7 @@
         if(!require(stats))                 
             warning("Could not load package stats")      
     }
+
+    cat("  Package", sQuote("exactRankTests"), "is no longer under development.\n",
+        " Please consider using package", sQuote("coin"), "instead.\n")
 }

bf2417c857ec0a58520a8a329fc5d885777ef39b
Torsten Hothorn <Torsten.Hothorn@R-project.org>
2011-08-31 00:00:00 +0000

    version 0.8-22

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 
 .onLoad <- function(lib, pkg) {
-    packageStartupMessage("Package", sQuote("exactRankTests"), 
+    packageStartupMessage(paste(" Package", sQuote("exactRankTests"), 
         "is no longer under development.\n",
-        "Please consider using package", sQuote("coin"), "instead.\n")
+        "Please consider using package", sQuote("coin"), "instead.\n"))
 }
